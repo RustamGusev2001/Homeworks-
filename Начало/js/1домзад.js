a48609f5e76d1df6989953989d26c83778ba2cd5
Gusev <rustik031rus@mail.ru>
2020-10-23 15:00:08 +0300

    третья контрольная точка

--- Начало/js/1домзад.js
+++ Начало/js/1домзад.js
@@ -1,6 +1,13 @@
 "use strict";
 
-const numberOfFilms = prompt("Сколько фильмов вы уже смотрели?", "");
+let numberOfFilms;
+function start() {
+    numberOfFilms = +prompt("Сколько фильмов вы уже смотрели?", "");
+    while( numberOfFilms =="" || numberOfFilms == null || isNaN(numberOfFilms)) {
+        numberOfFilms = +prompt("Сколько фильмов вы уже смотрели?", "");
+    }
+}
+start();
 const personalMovieDB = {
        count: numberOfFilms,
        movies: {},
@@ -8,10 +15,49 @@ const personalMovieDB = {
        genres: [],
        privat: false,
 };
- const name1 = prompt("Один из просмотренных фильмов?", ""),
-       rate1 = prompt("На сколько оцените его?", ""),
-       name2 = prompt("Один из просмотренных фильмов?", ""),
-       rate2 = prompt("На сколько оцените его?", "");
-personalMovieDB.movies[name1] = rate1;
-personalMovieDB.movies[name2] = rate2;
-console.log(personalMovieDB);
+function rememberMyFilms() {
+    let name1,
+    rate1,
+    i = 0;
+do {
+    name1 = prompt("Один из просмотренных фильмов?", "");
+    rate1 = prompt("На сколько оцените его?", "");
+    if (name1 != null && rate1 != null && name1.length < 50 && rate1.length <50 && name1 != "" && rate1 != "" ) {
+        personalMovieDB.movies[name1] = rate1;
+        console.log("done");
+        i++;
+    }
+    else { console.log("error");}
+} while(i<2);
+    
+}
+rememberMyFilms();
+function detectPersonalLvl() {
+    if (numberOfFilms < 10) {
+        alert("Посмотрено довольно мало фильмов");
+    }
+    else if (numberOfFilms<30 && numberOfFilms>=10) {
+        alert("Вы классический зритель");
+    }
+    else if (numberOfFilms>=30){
+        alert("Вы киноман!");
+    }
+    else {
+        alert("Произошла ошибка!");
+    }
+}
+detectPersonalLvl();
+function writeYourGenres() {
+    for ( let i = 0; i < 3; i++ ) {
+      let g = prompt(`Ваш любимый жанр № ${i+1}`, "");
+      personalMovieDB.genres[i] = g;
+    }
+}
+writeYourGenres();
+function showMyDB() {
+    if (personalMovieDB.privat != true) {
+        console.log(personalMovieDB);
+    }
+}
+showMyDB();
+
